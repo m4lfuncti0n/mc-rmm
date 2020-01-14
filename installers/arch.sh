e4a307213fbe157e06b2f42ca46c0952c9d8f13e
m4lfuncti0n <55944871+m4lfuncti0n@users.noreply.github.com>
2020-01-14 17:01:44 -0600

    Update arch.sh

--- installers/arch.sh
+++ installers/arch.sh
@@ -1,6 +1,6 @@
-UPDATE_URL="https://raw.githubusercontent.com/msmhq/msm/master"
-curl -L "${UPDATE_URL}/installers/common.sh" -o /tmp/msmcommon.sh  #wget isn't installed on Arch by default
-source /tmp/msmcommon.sh && rm -f /tmp/msmcommon.sh
+UPDATE_URL="https://raw.githubusercontent.com/m4lfuncti0n/mc-rmm/master"
+curl -L "${UPDATE_URL}/installers/common.sh" -o /tmp/mc-rmm.common.sh  #wget isn't installed on Arch by default
+source /tmp/mc-rmm.common.sh && rm -f /tmp/mc-rmm.common.sh
 
 function update_system_packages() {
     install_log "Updating sources"
@@ -14,24 +14,24 @@ function install_dependencies() {
 
 function enable_init() {
     install_log "Installing systemd service unit"
-    sudo wget ${UPDATE_URL}/init/msm.service \
-        -O /etc/systemd/system/msm.service
+    sudo wget ${UPDATE_URL}/init/mc-rmm.service \
+        -O /etc/systemd/system/mc-rmm.service
     
     install_log "Enabling automatic startup and shutdown"
-    sudo systemctl enable msm.service
+    sudo systemctl enable mc-rmm.service
 }
 
-# Verifies existence and permissions of msm server directory (default /opt/msm)
-function create_msm_directories() {
-    install_log "Creating MSM directories"
-    if [ ! -d "$msm_dir" ]; then
-        sudo mkdir -p "$msm_dir" || install_error "Couldn't create directory '$msm_dir'"
+# Verifies existence and permissions of mc-rmm server directory (default /opt/mc-rmm)
+function create_mc-rmm_directories() {
+    install_log "Creating mc-rmm directories"
+    if [ ! -d "$mc-rmm_dir" ]; then
+        sudo mkdir -p "$mc-rmm_dir" || install_error "Couldn't create directory '$mc-rmm_dir'"
     fi
-    sudo chown -R $msm_user:$msm_user "$msm_dir" || install_error "Couldn't change file ownership for '$msm_dir'"
+    sudo chown -R $mc-rmm_user:$mc-rmm_user "$mc-rmm_dir" || install_error "Couldn't change file ownership for '$mc-rmm_dir'"
     
     if [ ! -d "/etc/init.d" ]; then
         sudo mkdir -p "/etc/init.d/" || install_error "Couldn't create directory '/etc/init.d'"
     fi
 }
 
-install_msm
+install_mc-rmm
