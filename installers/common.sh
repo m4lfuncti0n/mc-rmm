a3e879d6cbb550598a7e5d0a92ebab1f6cca4347
m4lfuncti0n <55944871+m4lfuncti0n@users.noreply.github.com>
2020-01-14 17:02:30 -0600

    Update common.sh

--- installers/common.sh
+++ installers/common.sh
@@ -1,16 +1,16 @@
-msm_dir="/opt/msm"
-msm_user="minecraft"
-msm_user_system=false
-dl_dir="$(mktemp -d -t msm-XXX)"
+mc-rmm_dir="/opt/mc-rmm"
+mc-rmm_user="minecraft"
+mc-rmm_user_system=false
+dl_dir="$(mktemp -d -t mc-rmm-XXX)"
 
-# Outputs an MSM INSTALL log line
+# Outputs an mc-rmm INSTALL log line
 function install_log() {
-    echo -e "\n\033[1;32mMSM INSTALL: $*\033[m"
+    echo -e "\n\033[1;32mmc-rmm INSTALL: $*\033[m"
 }
 
-# Outputs an MSM INSTALL ERROR log line and exits with status code 1
+# Outputs an mc-rmm INSTALL ERROR log line and exits with status code 1
 function install_error() {
-    echo -e "\n\033[1;37;41mMSM INSTALL ERROR: $*\033[m"
+    echo -e "\n\033[1;37;41mmc-rmm INSTALL ERROR: $*\033[m"
     exit 1
 }
 
@@ -19,22 +19,22 @@ function install_error() {
 
 function config_installation() {
     install_log "Configure installation"
-    echo -n "Install directory [${msm_dir}]: "
+    echo -n "Install directory [${mc-rmm_dir}]: "
     read input
     if [ ! -z "$input" ]; then
-        msm_dir="$input"
+        mc-rmm_dir="$input"
     fi
 
-    echo -n "New server user to be created [${msm_user}]: "
+    echo -n "New server user to be created [${mc-rmm_user}]: "
     read input
     if [ ! -z "$input" ]; then
-        msm_user="$input"
+        mc-rmm_user="$input"
     fi
 
     echo -n "Add new user as system account? [y/N]: "
     read answer
     if [[ $answer != "y" ]]; then
-        msm_user_system=true
+        mc-rmm_user_system=true
     fi
 
     echo -n "Complete installation with these values? [y/N]: "
@@ -59,83 +59,83 @@ function install_dependencies() {
 
 # Verifies existence of or adds user for Minecraft server (default "minecraft")
 function add_minecraft_user() {
-    install_log "Creating default user '${msm_user}'"
-    if $msm_user_system; then
-        sudo useradd ${msm_user} --home "$msm_dir"
+    install_log "Creating default user '${mc-rmm_user}'"
+    if $mc-rmm_user_system; then
+        sudo useradd ${mc-rmm_user} --home "$mc-rmm_dir"
     else
-        sudo useradd ${msm_user} --system --home "$msm_dir"
+        sudo useradd ${mc-rmm_user} --system --home "$mc-rmm_dir"
     fi
 }
 
-# Verifies existence and permissions of msm server directory (default /opt/msm)
-function create_msm_directories() {
-    install_log "Creating MSM directories"
-    if [ ! -d "$msm_dir" ]; then
-        sudo mkdir -p "$msm_dir" || install_error "Couldn't create directory '$msm_dir'"
+# Verifies existence and permissions of mc-rmm server directory (default /opt/mc-rmm)
+function create_mc-rmm_directories() {
+    install_log "Creating mc-rmm directories"
+    if [ ! -d "$mc-rmm_dir" ]; then
+        sudo mkdir -p "$mc-rmm_dir" || install_error "Couldn't create directory '$mc-rmm_dir'"
     fi
-    sudo chown -R $msm_user:$msm_user "$msm_dir" || install_error "Couldn't change file ownership for '$msm_dir'"
+    sudo chown -R $mc-rmm_user:$mc-rmm_user "$mc-rmm_dir" || install_error "Couldn't change file ownership for '$mc-rmm_dir'"
 }
 
-# Fetches latest msm.conf, cron job, and init script
+# Fetches latest mc-rmm.conf, cron job, and init script
 function download_latest_files() {
     if [ ! -d "$dl_dir" ]; then
         install_error "Temporary download directory was not created properly"
     fi
 
-    install_log "Downloading latest MSM configuration file"
-    sudo wget ${UPDATE_URL}/msm.conf \
-        -O "$dl_dir/msm.conf.orig" || install_error "Couldn't download configuration file"
+    install_log "Downloading latest mc-rmm configuration file"
+    sudo wget ${UPDATE_URL}/mc-rmm.conf \
+        -O "$dl_dir/mc-rmm.conf.orig" || install_error "Couldn't download configuration file"
 
-    install_log "Downloading latest MSM cron file"
-    sudo wget ${UPDATE_URL}/cron/msm \
-        -O "$dl_dir/msm.cron.orig" || install_error "Couldn't download cron file"
+    install_log "Downloading latest mc-rmm cron file"
+    sudo wget ${UPDATE_URL}/cron/mc-rmm \
+        -O "$dl_dir/mc-rmm.cron.orig" || install_error "Couldn't download cron file"
 
-    install_log "Downloading latest MSM version"
-    sudo wget ${UPDATE_URL}/init/msm \
-        -O "$dl_dir/msm.init.orig" || install_error "Couldn't download init file"
+    install_log "Downloading latest mc-rmm version"
+    sudo wget ${UPDATE_URL}/init/mc-rmm \
+        -O "$dl_dir/mc-rmm.init.orig" || install_error "Couldn't download init file"
 }
 
-# Patches msm.conf and cron job to use specified username and directory
+# Patches mc-rmm.conf and cron job to use specified username and directory
 function patch_latest_files() {
     # patch config file
-    install_log "Patching MSM configuration file"
-    sudo sed 's#USERNAME="minecraft"#USERNAME="'$msm_user'"#g' "$dl_dir/msm.conf.orig" | \
-        sed "s#/opt/msm#$msm_dir#g" | \
-        sed "s#UPDATE_URL=.*\$#UPDATE_URL=\"$UPDATE_URL\"#" >"$dl_dir/msm.conf"
+    install_log "Patching mc-rmm configuration file"
+    sudo sed 's#USERNAME="minecraft"#USERNAME="'$mc-rmm_user'"#g' "$dl_dir/mc-rmm.conf.orig" | \
+        sed "s#/opt/mc-rmm#$mc-rmm_dir#g" | \
+        sed "s#UPDATE_URL=.*\$#UPDATE_URL=\"$UPDATE_URL\"#" >"$dl_dir/mc-rmm.conf"
 
     # patch cron file
-    install_log "Patching MSM cron file"
-    sudo awk '{ if ($0 !~ /^#/) sub(/minecraft/, "'$msm_user'"); print }' \
-        "$dl_dir/msm.cron.orig" >"$dl_dir/msm.cron"
+    install_log "Patching mc-rmm cron file"
+    sudo awk '{ if ($0 !~ /^#/) sub(/minecraft/, "'$mc-rmm_user'"); print }' \
+        "$dl_dir/mc-rmm.cron.orig" >"$dl_dir/mc-rmm.cron"
 
     # patch init file
-    install_log "Patching MSM init file"
-    sudo cp "$dl_dir/msm.init.orig" "$dl_dir/msm.init"
+    install_log "Patching mc-rmm init file"
+    sudo cp "$dl_dir/mc-rmm.init.orig" "$dl_dir/mc-rmm.init"
 }
 
-# Installs msm.conf into /etc
+# Installs mc-rmm.conf into /etc
 function install_config() {
-    install_log "Installing MSM configuration file"
-    sudo install -b -m0644 "$dl_dir/msm.conf" /etc/msm.conf
-    if [ ! -e /etc/msm.conf ]; then
+    install_log "Installing mc-rmm configuration file"
+    sudo install -b -m0644 "$dl_dir/mc-rmm.conf" /etc/mc-rmm.conf
+    if [ ! -e /etc/mc-rmm.conf ]; then
         install_error "Couldn't install configuration file"
     fi
 }
 
-# Installs msm.cron into /etc/cron.d
+# Installs mc-rmm.cron into /etc/cron.d
 function install_cron() {
-    install_log "Installing MSM cron file"
-    sudo install -m0644 "$dl_dir/msm.cron" /etc/cron.d/msm || install_error "Couldn't install cron file"
+    install_log "Installing mc-rmm cron file"
+    sudo install -m0644 "$dl_dir/mc-rmm.cron" /etc/cron.d/mc-rmm || install_error "Couldn't install cron file"
     sudo /etc/init.d/cron reload
 }
 
 # Installs init script into /etc/init.d
 function install_init() {
-    install_log "Installing MSM init file"
-    sudo install -b "$dl_dir/msm.init" /etc/init.d/msm || install_error "Couldn't install init file"
+    install_log "Installing mc-rmm init file"
+    sudo install -b "$dl_dir/mc-rmm.init" /etc/init.d/mc-rmm || install_error "Couldn't install init file"
 
-    install_log "Making MSM accessible as the command 'msm'"
-    sudo ln -s /etc/init.d/msm /usr/local/bin/msm
+    install_log "Making mc-rmm accessible as the command 'mc-rmm'"
+    sudo ln -s /etc/init.d/mc-rmm /usr/local/bin/mc-rmm
 }
 
 # Enables init script in default runlevels
@@ -144,35 +144,35 @@ function enable_init() {
     install_error "No function defined for enable_init"
 }
 
-# Updates rest of MSM using init script updater
-function update_msm() {
-    install_log "Asking MSM to update itself"
-    sudo /etc/init.d/msm update --noinput
+# Updates rest of mc-rmm using init script updater
+function update_mc-rmm() {
+    install_log "Asking mc-rmm to update itself"
+    sudo /etc/init.d/mc-rmm update --noinput
 }
 
-# Updates rest of MSM using init script updater
+# Updates rest of mc-rmm using init script updater
 function setup_jargroup() {
     install_log "Setup default jar groups"
-    sudo /etc/init.d/msm jargroup create minecraft minecraft
+    sudo /etc/init.d/mc-rmm jargroup create minecraft minecraft
 }
 
 function install_complete() {
-    install_log "Done. Type 'msm help' to get started. Have fun!"
+    install_log "Done. Type 'mc-rmm help' to get started. Have fun!"
 }
 
-function install_msm() {
+function install_mc-rmm() {
     config_installation
     add_minecraft_user
     update_system_packages
     install_dependencies
-    create_msm_directories
+    create_mc-rmm_directories
     download_latest_files
     patch_latest_files
     install_config
     install_cron
     install_init
     enable_init
-    update_msm
+    update_mc-rmm
     setup_jargroup
     install_complete
 }
